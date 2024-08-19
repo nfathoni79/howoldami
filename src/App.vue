038ea65ecaac29d656a31e6744bc847fa6db922d
NN Fathoni <naufalfathoni79@gmail.com>
2024-08-19 16:58:21 +0700

    Add age in months and days

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,8 @@ import { useRouter, useRoute } from 'vue-router'
 const router = useRouter()
 const route = useRoute()
 
+const nowDate = new Date().toISOString().split('T')[0]
+
 const birthDate = computed({
   get: () => {
     let year = route.query.y
@@ -35,9 +37,42 @@ const birthDate = computed({
 
 const age = computed(() => {
   const now = new Date()
-  const diff = now - new Date(birthDate.value)
+  const birth = new Date(birthDate.value)
 
-  return Math.floor(diff / 31536000000)
+  if (now < birth) return null
+
+  const nowDay = now.getDate()
+  let nowMonth = now.getMonth() + 1
+  let nowYear = now.getFullYear()
+  const birthDay = birth.getDate()
+  const birthMonth = birth.getMonth() + 1
+  const birthYear = birth.getFullYear()
+
+  let ageDay = 0
+
+  if (nowDay < birthDay) {
+    nowMonth -= 1
+    ageDay = nowDay + 31 - birthDay
+  } else {
+    ageDay = nowDay - birthDay
+  }
+
+  let ageMonth = 0
+
+  if (nowMonth < birthMonth) {
+    nowYear -= 1
+    ageMonth = nowMonth + 12 - birthMonth
+  } else {
+    ageMonth = nowMonth - birthMonth
+  }
+
+  const ageYear = nowYear - birthYear
+
+  return {
+    year: ageYear,
+    month: ageMonth,
+    day: ageDay,
+  }
 })
 
 const currentYear = new Date().getFullYear()
@@ -46,14 +81,20 @@ const currentYear = new Date().getFullYear()
 <template>
   <div class="mx-auto max-w-sm min-h-screen
     flex flex-col justify-center gap-8 px-4 py-8 text-center text-gray-800">
-    <input type="date" v-model="birthDate"
+    <input type="date" :max="nowDate" v-model="birthDate"
       class="block mx-auto text-center
       focus:border-green-500 focus:ring-green-500" />
 
-    <div class="flex flex-col gap-4">
+    <div v-if="age" class="flex flex-col gap-4">
       <p class="text-4xl font-semibold uppercase">I am</p>
-      <p class="text-8xl font-bold">{{ age }}</p>
-      <p class="text-4xl font-semibold uppercase">years old</p>
+      <p class="text-8xl font-bold">{{ age.year }}</p>
+      <p class="text-4xl font-semibold uppercase">years</p>
+      <p class="font-medium uppercase">
+        {{ age.month }} months {{ age.day }} days old
+      </p>
+    </div>
+    <div v-else>
+      <p>Birth date needs to be earlier than today</p>
     </div>
 
     <p>
